289ec3eed831988cc9be2e0969080c11eb951feb
nmskm240 <UTC.AM240@gmail.com>
2023-11-09 14:14:04 +0000

    関数化

--- app/nbot/src/command/members.py
+++ app/nbot/src/command/members.py
@@ -18,27 +18,14 @@ class Members(commands.GroupCog, group_name="members"):
     @app_commands.command(name="import", description="名簿をCSVからインポート")
     async def import_csv(self, interaction: discord.Interaction, csv_file: discord.Attachment):
         logger.info(f"execute namelist command by {interaction.user.id}")
+        
         await interaction.response.defer(thinking=True)
-        try: 
-            await csv_file.save(".//temp.csv")
-            with open("temp.csv", "r") as file:
-                reader = csv.DictReader(file)
-                rows = [row for row in reader]
-                if "name" not in rows[0] or \
-                    "student_id" not in rows[0] or \
-                    "discord_id" not in rows[0]:
-                    raise KeyError()
-        except csv.Error as e:
-            logger.error("unsupported file")
-            await interaction.followup.send("サポートされていないファイル形式です。\nCSV形式に変換して再実行してください。", silent = True)
+        res, rows = await try_csv_parse(csv_file)
+
+        if(res):
+            await interaction.followup.send(res, silent=True)
             return
-        except KeyError as e:
-            logger.error("unsupported csv file")
-            await interaction.followup.send("csvファイルにname、student_id, discord_idいずれかのヘッダーが存在しません。\nヘッダーを見直して再実行してください。", silent = True)
-        except:
-            logger.error("unhandler error")
-            await interaction.followup.send("想定外のエラーが発生しました。")
-            return
+        
         state = sqlite.insert(datamodel.Member).values([
             dict(
                 student_id = x["student_id"],
@@ -53,22 +40,12 @@ class Members(commands.GroupCog, group_name="members"):
                 discord_id=state.excluded.discord_id
             ),
         )
-        is_success = True
-        try: 
-            session = create_session()
-            session.execute(state)
-            session.commit()
-        except:
-            logger.error("unhandler error")
-            is_success = False
-            session.rollback()
-        finally:
-            session.close()
-        if(is_success):
-            await interaction.followup.send("データベースを更新しました。")
+        is_complete = await try_execute_statement(state)
+        if is_complete:
+            res_text = "データベースを更新しました。" 
         else:
-            await interaction.followup.send("データベースの更新に失敗しました。\n時間を置いて再度実行してください。")
-    
+            res_text = "データベースの更新に失敗しました。\n時間を置いて再度実行してください。"
+        await interaction.followup.send(res_text)
 
     @app_commands.command(name="remove", description="削除")
     async def remove(self, interaction: discord.Interaction):
@@ -77,6 +54,42 @@ class Members(commands.GroupCog, group_name="members"):
     @app_commands.command(name="export", description="名簿出力")
     async def export(self, interaction: discord.Interaction):
         await interaction.response.send_message("export")
+
+async def try_execute_statement(state: sqlite.Insert) -> bool:
+    is_complet = True
+    try: 
+        session = create_session()
+        session.execute(state)
+        session.commit()
+    except:
+        logger.error("unhandler error")
+        session.rollback()
+        is_complet = False
+    finally:
+        session.close()
+    return is_complet
+
+async def try_csv_parse(csv_file: discord.Attachment) -> tuple[str, list[dict[str]]]:
+    try: 
+        await csv_file.save(".//temp.csv")
+        with open("temp.csv", "r") as file:
+            reader = csv.DictReader(file)
+            rows = [row for row in reader]
+            if "name" not in rows[0] or \
+                "student_id" not in rows[0] or \
+                "discord_id" not in rows[0]:
+                raise KeyError()
+    except csv.Error as e:
+        logger.error("unsupported file")
+        return "サポートされていないファイル形式です。\nCSV形式に変換して再実行してください。", []
+    except KeyError as e:
+        logger.error("unsupported csv file")
+        return "csvファイルにname、student_id, discord_idいずれかのヘッダーが存在しません。\nヘッダーを見直して再実行してください。", []
+    except:
+        logger.error("unhandler error")
+        return "想定外のエラーが発生しました。", []
+    else:
+        return "", rows
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Members(bot), guild=discord.Object(id="853968633340100648"))
